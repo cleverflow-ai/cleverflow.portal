185df0d256c9ff8e4dec2542b2a2638bce69fbe1
Lan Luu <lan.luu@clevernow.com>
2025-05-14 11:08:52 +0200

    Ignored .astro folder; Added MermaildJS for Diagrams.

--- .astro/content-modules.mjs
+++
@@ -1,7 +0,0 @@
-
-export default new Map([
-["src/content/docs/backup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbackup.mdx&astroContentModuleFlag=true")],
-["src/content/docs/concepts/breakthrough.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fconcepts%2Fbreakthrough.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdoc&astroContentModuleFlag=true")],
-["src/content/docs/use-cases/machinery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fuse-cases%2Fmachinery.mdx&astroContentModuleFlag=true")]]);
-		
